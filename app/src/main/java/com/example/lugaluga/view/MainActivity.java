4bdcc8143fae052706da13f5a6dbbe8409a233d4
Rafael <rafaelleitaolima@gmail.com>
2024-04-30 16:52:18 -0300

    Aula dia 03/04

---
+++ app/src/main/java/com/example/lugaluga/view/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.lugaluga.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.lugaluga.R;
+import com.example.lugaluga.model.Produto;
+import com.example.lugaluga.view.adapter.AdapterProduto;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private AdapterProduto adapterProduto;
+    private List<Produto> produtoList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView.findViewById(R.id.listaProdutos);
+
+    }
+    public void CriarListaProdutos(){
+        Produto produto = new Produto(
+                "Iphone 13",
+                "Iphone 13 64Gb",
+                200.00,
+                10,
+                "Disponível");
+
+    }
+
+}
